72d9b22919b7c8753d0409b497c78d54258dfe80
MaxwellW32 <maxwellwedderburn32@gmail.com>
2025-05-19 21:23:54 -0500

    latest

--- out/layout.js
+++ out/layout.js
@@ -1,10 +1,10 @@
 import { loadComponent } from "./utility.js";
 async function layout() {
     //get navbar
-    loadComponent({ componentLocation: "components/navbar/navbar.html", elSelector: "#nav" });
+    loadComponent({ componentLocation: "components/navbar/navbar.html", elSelector: "#navTemplate" });
     //get sidebar
-    loadComponent({ componentLocation: "components/sidebar/sidebar.html", elSelector: "#sidebar" });
+    loadComponent({ componentLocation: "components/sidebar/sidebar.html", elSelector: "#sidebarTemplate" });
     //get footer
-    loadComponent({ componentLocation: "components/footer/footer.html", elSelector: "#footer" });
+    loadComponent({ componentLocation: "components/footer/footer.html", elSelector: "#footerTemplate" });
 }
 layout();
